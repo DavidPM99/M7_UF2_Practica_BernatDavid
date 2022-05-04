b9225bf1ab8b063600a22c2a1d5dc0442d3f0e6b
Davugger <102175993+DavidPM99@users.noreply.github.com>
2022-05-04 15:27:26 +0200

    push

---
+++ GestioEmocionalCat/app/src/androidTest/java/com/example/gestioemocionalcat/AvaluacioTest4.kt
@@ -0,0 +1,159 @@
+package com.example.gestioemocionalcat
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class AvaluacioTest4 {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun avaluacioTest4() {
+        val appCompatButton = onView(
+            allOf(
+                withId(R.id.bt_avalua), withText("AVALUA EL TEU ESTAT"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton.perform(click())
+
+        val appCompatButton2 = onView(
+            allOf(
+                withId(android.R.id.button1), withText("D'acord"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.buttonPanel),
+                        0
+                    ),
+                    3
+                )
+            )
+        )
+        appCompatButton2.perform(scrollTo(), click())
+
+        val appCompatButton3 = onView(
+            allOf(
+                withId(R.id.bt_resposta1), withText("Cap dia"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.aval),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton3.perform(click())
+
+        val appCompatButton4 = onView(
+            allOf(
+                withId(R.id.bt_resposta1), withText("Cap dia"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.aval2),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton4.perform(click())
+
+        val appCompatButton5 = onView(
+            allOf(
+                withId(R.id.bt_resposta1), withText("Cap dia"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.aval3),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatButton5.perform(click())
+
+        val actionMenuItemView = onView(
+            allOf(
+                withId(R.id.menu_fet), withText("FET"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.action_bar),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        actionMenuItemView.perform(click())
+
+        val imageView = onView(
+            allOf(
+                withId(R.id.im_salut),
+                withParent(withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))),
+                isDisplayed()
+            )
+        )
+        imageView.check(matches(isDisplayed()))
+
+        val imageView2 = onView(
+            allOf(
+                withId(R.id.im_gencat),
+                withParent(withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))),
+                isDisplayed()
+            )
+        )
+        imageView2.check(matches(isDisplayed()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}

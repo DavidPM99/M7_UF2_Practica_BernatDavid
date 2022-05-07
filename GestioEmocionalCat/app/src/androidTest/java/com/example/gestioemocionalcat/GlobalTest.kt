7af030c635049d98b3421d0c3250bebf85c38dfe
Davugger <102175993+DavidPM99@users.noreply.github.com>
2022-05-07 11:46:10 +0200

    aa
    aa

--- GestioEmocionalCat/app/src/androidTest/java/com/example/gestioemocionalcat/GlobalTest.kt
+++ GestioEmocionalCat/app/src/androidTest/java/com/example/gestioemocionalcat/GlobalTest.kt
@@ -1,13 +1,14 @@
 package com.example.gestioemocionalcat
 
-import org.junit.Assert.*
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(BasicTest::class,
-    RecursosTest::class, AvaluacioTest1::class,
-    AvaluacioTest2::class, AvaluacioTest4::class)
+AvaluacioTest1::class,
+AvaluacioTest2::class,
+AvaluacioTest4::class,
+RecursosTest::class)
 
 class GlobalTest {}
